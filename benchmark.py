3c576fdb5954f1728be0a126f7b8548a4014e9a7
Bagus Hanindhito <hanindhito@bagus.my.id>
2020-06-22 23:48:21 -0400

    Implementing multiple precision mode, refactoring code

--- benchmark.py
+++ benchmark.py
@@ -1,38 +1,105 @@
 import torch
 import time
+import argparse
 
-GPU = True
-INFINITY = False
+def generate_matrices(matrix_size, data_format):
+    #print("Generating Random Matrix", flush=True)
+    matrix = torch.rand(matrix_size, matrix_size, dtype=torch.float64)
 
-# preload the matrices
-print("Preloading matrices...", flush=True)
-mats = {}
-MATSZ = [100, 500, 1000, 2000, 4000, 8000, 16000, 32000]
-for msz in MATSZ:
-    mat = torch.rand(msz, msz)
-    if GPU:
-        mats[msz] = mat.cuda()
+    # casting fp64 tensor as needed.
+    if(data_format=='fp64'):
+        matrix=matrix
+    elif(data_format=='fp32'):
+        matrix=matrix.float()
+    elif(data_format=='fp16'):
+        matrix=matrix.half()
+    elif(data_format=='int64'):
+        matrix=matrix.long()
+    elif(data_format=='int32'):
+        matrix=matrix.int()
+    elif(data_format=='int16'):
+        matrix=matrix.short()
+    elif(data_format=='int8'):
+        matrix=matrix.char()
     else:
-        mats[msz] = mat
-print("Done preloading.", flush=True)
+        print("Non-supported Data Type.")
+        exit()
 
-def run_benchmark(msz):
-    ops = 2*msz**3
+    #print("Matrix Generation is done", flush=True)
+    return(matrix)
+
+def run_benchmark(matrix, matrix_size, useCUDA):
+
+    # Calculating Number of Ops
+    ops = 2*matrix_size**3
+    # Copying matrix to GPU memory (if GPU is used)
+    if(useCUDA==True):
+        #print("Copying Matrix to GPU Memory", flush=True)
+        matrix_GPU = matrix.cuda()
+    else:
+        matrix_GPU = matrix
+    
+    # Take a note for start time
     start = time.time()
-    mat = mats[msz]
-    r = torch.mm(mat, mat).cuda()
-    if GPU:
-        torch.cuda.synchronize()
-    end = time.time()
-    duration = end - start
-    tflops = ops / duration / 10**12
-    print("{}x{} MM {} ops in {} sec = TFLOPS {}"
-          .format(msz, msz, ops, duration, tflops),
-          flush=True)
 
-while True:
-    print("======== Results ======== ", flush=True)
-    for msz in MATSZ:
-        run_benchmark(msz)
-    if not INFINITY:
-        break
+    # Begin Multiplication
+    #print("Multiplying Matrices", flush=True)
+    result = torch.mm(matrix_GPU, matrix_GPU).cuda()
+
+    # Wait operation to finish
+    if(useCUDA==True):
+        torch.cuda.synchronize()
+
+    # Take a note for end time
+    end = time.time()
+
+    # Calculate Elapsed Time
+    duration = end - start
+
+    # Calculate TFLOPS
+    tflops = ops / duration / 10**12
+    print("{}x{} MM {} ops in {} sec = TFLOPS {}".format(matrix_size, matrix_size, ops, duration, tflops), flush=True)
+
+    # Clean-up
+    #print("Cleaning-up", flush=True)
+    if(useCUDA==True):
+        del matrix_GPU
+        torch.cuda.empty_cache()
+
+def cmdline_args():
+    # Make parser object
+    parser = argparse.ArgumentParser(description='PyTorch CUDA Matrix Multiplication Benchmark.')
+    parser.add_argument("--useCUDA", type=bool, default=False, help='Use CUDA-capable GPU for Benchmark.', choices=[True, False])
+    parser.add_argument("--precision", type=str, default='fp32', help='Data Precision for Benchmark', choices=['int8', 'int16', 'int32', 'int64', 'fp16', 'fp32', 'fp64'])
+    parser.add_argument("--loop", type=bool, default=False, help='Run benchmark infinite times until manually cancelled.', choices=[True, False])
+    parser.add_argument("--size", type=str, default='100, 500, 1000, 2000, 4000, 8000, 16000, 32000', help='List of matrices size separated by comma, e.g., 100, 500, 1000')
+    return(parser.parse_args())
+
+
+if __name__ == '__main__':
+    
+    # Parse Arguments
+    try:
+        args = cmdline_args()
+        #print(args)
+    except:
+        print("Launch argument error!")
+        print("Example: $python <script_name> --useCUDA=True --precision='fp32' --size='100, 500, 1000'")
+        exit()
+
+    # Parse Matrices Size
+    try:
+        size_list = [int(i) for i in args.size.split(",")]
+    except:
+        print("Invalid list of matrix size. Use only integer separated by comma to define the list of matrix size.")
+        exit()
+
+    while True:
+        for matrix_size in size_list:
+            matrix = generate_matrices(matrix_size,args.precision)
+            run_benchmark(matrix, matrix_size, args.useCUDA)
+            del matrix
+        if (args.loop==False):
+            break
+    
+   
